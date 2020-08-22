0ff83b73832b6ff814ab585dcc7fe16fa9b5f72b
Witthawat Leethong <48642719+bankwitthawat@users.noreply.github.com>
2020-08-22 13:20:38 +0700

    new repo

---
+++ string2intChallenge/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string2intChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input your string: ");
+
+            //string str2int = 
+            string a = Console.ReadLine();
+            string b = string.Empty;
+            int val;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Char.IsDigit(a[i]))
+                    b += a[i];
+            }
+
+            if (b.Length > 0)
+            {
+                val = int.Parse(b);
+                Console.WriteLine("number is: " + val);
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("error...");
+                Console.Read();
+            }
+
+
+        }
+    }
+}
