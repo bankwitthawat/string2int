2273f3cf6ba92e48f0e57939a887aed0343f7508
วิทวัตย์ ลี้ทอง <Lee.Witthawat@outlook.co.th>
2020-08-22 13:44:30 +0700

    update with linq

--- string2intChallenge/Program.cs
+++ string2intChallenge/Program.cs
@@ -12,28 +12,40 @@ namespace string2intChallenge
         {
             Console.WriteLine("please input your string: ");
 
-            string a = Console.ReadLine();
-            string b = string.Empty;
-            int val;
+            string input = Console.ReadLine();
+            string result = new String(input.Where(Char.IsDigit).ToArray());
 
-            for (int i = 0; i < a.Length; i++)
-            {
-                if (Char.IsDigit(a[i]))
-                    b += a[i];
-            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("your digi is " + int.Parse(result));
+            Console.Read();
 
-            if (b.Length > 0)
-            {
-                val = int.Parse(b);
-                Console.WriteLine("number is: " + val);
-                Console.Read();
-            }
-            else
-            {
-                Console.WriteLine("error...");
-                Console.Read();
-            }
 
+
+
+            //Console.WriteLine("please input your string: ");
+
+            //string a = Console.ReadLine();
+            //string b = string.Empty;
+            //int val;
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    if (Char.IsDigit(a[i]))
+            //        b += a[i];
+            //}
+
+            //if (b.Length > 0)
+            //{
+            //    val = int.Parse(b);
+            //    Console.WriteLine("number is: " + val);
+            //    Console.Read();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("error...");
+            //    Console.Read();
+            //}
 
         }
     }
